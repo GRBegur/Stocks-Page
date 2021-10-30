0a245f37897fb1b40a897ee3cbdcc1db1bd27b82
guna begur <gunarbegur.gmail.com>
2021-10-30 13:00:53 +0530

    Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
+import Header from './components/Header';
+import './style/App.css'
 
 function App() {
   return (
-    <header>
-      
-    </header>
+   <Header />
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+function Header(){
+    return(
+        <header>
+        <nav>
+          <h1><span className="pre-heading">Information on</span> Stocks</h1>    
+        </nav>
+      </header>
+    )
+}
+
+export default Header
